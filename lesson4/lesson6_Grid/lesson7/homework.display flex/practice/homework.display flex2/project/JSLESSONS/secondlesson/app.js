9da480eb65a3f7cc558eb6caa7988119d64ea8fa
saniyaforgit <saniyaforgit@gmail.com.com>
2026-03-20 13:15:12 +0500

    lesson1

---
+++ lesson4/lesson6_Grid/lesson7/homework.display flex/practice/homework.display flex2/project/JSLESSONS/secondlesson/app.js
@@ -0,0 +1,129 @@
+// let i =1
+// while (i <=5){
+//     console.log(i)
+//     i+++
+// }
+
+// let i =10
+// while (i <=1){
+//     console.log(i)
+//     i--
+// }
+
+
+
+// let i =2
+// while (i <=20){
+//     console.log(i)
+//     i+=2
+// }
+
+// let i = 1
+// let sum =0 
+
+// while (i<=5){
+//     sum +=1
+//     i++
+// }
+// console.log(sum)
+
+// let input =""
+
+// while (input!=="exit")
+//     input= prompt("Введите для exit для выхода")
+
+// let number=Number(prompt("Введите больше 10"))
+
+// while (number<=10){
+//     number=Number (prompt("Ошибка, Введите снова"))
+// }
+
+// for (let i =1; i <=5; i++){
+//     console.log(i)
+// }
+
+// for (let i =1; i <=5; i >=1; i--){
+//     console.log(i)
+// }
+
+// for (let i =1; i >=20; i+=5){
+//     console.log(i)
+// }
+
+// for (let i =1; i<10; i++){
+//     sum +=i
+// }
+// console.log(sum)
+
+// let i =1
+// do {
+//     console.log(i)
+//     i++
+// }
+// while(false)
+
+// let choice
+
+// do{
+//     choice = prompt("1 - привет, 2- выход")
+//     if(choice =="1")
+// }
+
+// let choicedo{
+//     choice=prompt("1-Привет, 2- Выход")
+//     if (choice === "1"){
+//         console.log("Привет")
+//     }
+// }
+// while (choice !=="2")
+
+
+// for (let i=1; i<=3; i++){
+//     for (let j =1; j<=3; j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for (int i=1; i <= 3; i++){
+//     let row =""
+
+//     for (let j=1; j <=3; j++){
+//         row += "*"
+//     }
+//     console.log (row)
+// }
+
+// for (let i=1; <=5; i++){
+//     let row =""
+//     for (let j =1; j<=i; j++){
+//         row +="*"
+//     }
+//     console.log(row)
+// }
+
+// for (let i= 0; i<8; i++){
+//     let row =""
+//     for (let j=0; j<8; j++){
+//         if ((i=j)%2===0){
+//             row+="⬛"
+//         }
+//         else{
+//             row +="⬜"
+//         }
+//     }
+//     console.log (row)
+// }
+
+let i =1
+while (i<=10){
+    if (i % 2!==0){
+        console.log(i)
+    }
+    i ++    
+}
+
+for (let i =1; i <=10; i++){
+    if (i %3 === 0){
+        console.log(i)
+    }
+}
